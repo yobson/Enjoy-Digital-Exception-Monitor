026fc3aa540aae83f7e3ef1d33bf2ba984bf6704
James Hobson <jam.hobson@hotmail.co.uk>
2016-08-26 17:09:47 +0100

    Started Making the GUI in object for and not function form

---
+++ Enjoy Digital Exception Monitor/GUI/Menu.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enjoy_Digital_Exception_Monitor.GUI
+{
+    class Menu
+    {
+        private IEnumerable<MenuItem> Listings;
+        private IEnumerable<string> Menus;
+        private int selected = 1;
+
+        public void AddListing(MenuItem i)
+        {
+            List<MenuItem> l = Listings.ToList();
+            l.Add(i);
+            Listings = l.AsEnumerable<MenuItem>();
+        }
+
+        public void AddMenu(string s)
+        {
+            List<string> l = Menus.ToList();
+            l.Add(s);
+            Menus = l.AsEnumerable<string>();
+        }
+
+        private void PrintMenu()
+        {
+            Console.Clear();
+            int position = 1;
+            foreach (string menuTitle in Menus)
+            {
+                foreach (MenuItem M in Listings.Where(x => x.Menu == menuTitle))
+                {
+                    M.position = position;
+                    M.Show(null, selected);
+                    position++;
+                }
+            }
+        }
+    }
+}
---
+++ Enjoy Digital Exception Monitor/GUI/MenuItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enjoy_Digital_Exception_Monitor.GUI
+{
+    class MenuItem : Menu
+    {
+        public string ListingText { get; set; }
+        public string PropertyName { get; set; }
+        public int Type { get; set; }
+        public string Menu { get; set; }
+        public string DefaltValue = "";
+        public const int String = 0;
+        public const int Bool = 1;
+        public int position;
+
+        public Crawling ChangeValue(Crawling crawl)
+        {
+            switch (this.Type)
+            {
+                case (0): //If String
+                    Console.Write("\nNew Value: ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    string newValue = Console.ReadLine();
+                    crawl.GetType().GetProperty(PropertyName).SetValue(crawl, newValue);
+                    break;
+                case (1):
+                    break;
+            }
+            return crawl;
+        }
+
+        public void Show(Crawling crawl, int selected)
+        {
+            if (selected == position) { Console.BackgroundColor = ConsoleColor.White; Console.ForegroundColor = ConsoleColor.Black; }
+            Console.WriteLine(ListingText + ": " + crawl.GetType().GetProperty(PropertyName).ToString());
+            Console.ResetColor();
+        }
+    }
+}
